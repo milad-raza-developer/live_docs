f7ec8c8c9c0bd10ba01b72db859cd336f009d455
MILAD RAZA <56252911+milad-raza-developer@users.noreply.github.com>
2024-08-15 04:40:26 -0700

    integration and authetication setup of liveblocks

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react'
+import { ClientSideSuspense, RoomProvider } from '@liveblocks/react'
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
+const CollaborativeRoom = () => {
+    return (
+        <RoomProvider id="my-room">
+            <ClientSideSuspense fallback={<div>Loading…</div>}>
+                <Header>
+                    <div className='flex w-fit items-center justify-center gap-2'>
+                        <p className='document-title'>Share</p>
+                    </div>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </Header>
+                <Editor />
+            </ClientSideSuspense>
+        </RoomProvider>
+    )
+}
+
+export default CollaborativeRoom
